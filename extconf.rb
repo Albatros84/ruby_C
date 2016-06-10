a12a2f6b5648bb633f00bd0681665bdacf21dab9
janusz.liro <janusz.liro@mobica.com>
2016-06-10 15:23:43 +0200

    initial commit

---
+++ extconf.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+require 'mkmf'
+
+# preparation for compilation goes here
+
+create_header
+create_makefile 'foobar'
+
+
+
+
+
+
---
+++ foobar.c
@@ -0,0 +1,7 @@
+#include <ruby.h>
+
+void Init_foobar()
+{
+  printf("Hello World \n");
+  /* code run by `require` */
+}
